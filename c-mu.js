fb60276366607382a01d0afd8f5954fa9c2badb5
delemoh79912 <167154350+delemoh79912@users.noreply.github.com>
2024-05-15 21:46:14 +0330

    Create c-mu.js

---
+++ c-mu.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    const container = $('#container');
+    const parent = $('#parent');
+    const follower = $('#follower');
+    const clipSize = 160;
+
+    parent.mousemove(function(e) {
+      const containerRect = container[0].getBoundingClientRect();
+      const parentRect = parent[0].getBoundingClientRect();
+      const mouseX = e.clientX - parentRect.left;
+      const mouseY = e.clientY - parentRect.top;
+      
+
+      const clipX = (mouseX / parentRect.width) * 100;
+      const clipY = (mouseY / parentRect.height) * 100;
+
+      container.css('clip-path', `circle(80px at ${clipX}% ${clipY}%)`);
+      
+      follower.css('left', mouseX - clipSize / 2 + 'px');
+      follower.css('top', mouseY - clipSize / 2 + 'px');
+    });
+  });
