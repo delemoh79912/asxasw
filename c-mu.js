2709a1ab299ad8f228e63268b45043d8e1b1b39c
delemoh79912 <167154350+delemoh79912@users.noreply.github.com>
2024-05-16 14:04:51 +0330

    Update c-mu.js

--- c-mu.js
+++ c-mu.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     const container = $('#container');
     const parent = $('#parent');
     const follower = $('#follower');
-    const clipSize = 160;
+    const clipSize = 200;
 
     parent.mousemove(function(e) {
       const containerRect = container[0].getBoundingClientRect();
@@ -14,7 +14,7 @@ $(document).ready(function() {
       const clipX = (mouseX / parentRect.width) * 100;
       const clipY = (mouseY / parentRect.height) * 100;
 
-      container.css('clip-path', `circle(80px at ${clipX}% ${clipY}%)`);
+      container.css('clip-path', `circle(100px at ${clipX}% ${clipY}%)`);
       
       follower.css('left', mouseX - clipSize / 2 + 'px');
       follower.css('top', mouseY - clipSize / 2 + 'px');
